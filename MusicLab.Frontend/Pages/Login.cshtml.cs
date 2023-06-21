2c8e5a7ca60ffb645fab65bc389cc212041aab27
Ngo Manh Dat (k15 HL) <datnmhe153061@fpt.edu.vn>
2023-06-21 12:45:43 +0700

    update layout login

---
+++ MusicLab.Frontend/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MusicLab.Frontend.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
